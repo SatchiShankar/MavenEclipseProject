3be9551ee7c41636725cb7bce0af4bbb630c62dc
SatchiShankar <SatchiShankar@gmail.com>
2021-12-09 20:51:36 -0500

    Commit  851 pm

--- MavenEclipseProject/src/test/java/Satchi/MavenEclipseProject/AppTest.java
+++ MavenEclipseProject/src/test/java/Satchi/MavenEclipseProject/AppTest.java
@@ -15,7 +15,7 @@ public class AppTest
     @Test
     public void shouldAnswerWithTrue()
     {
-    	System.out.println("Testing");
+    	System.out.println("Testing12345");
         assertTrue( true );
     	System.out.println("Testing12345");
 
